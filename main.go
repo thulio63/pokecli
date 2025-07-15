70fe75219f3c525e3b0ff6a9f71edc0894f5158b
thulio63 <thulio63@gmail.com>
2025-07-14 19:35:04 -0500

    REPL can now recognize commands 'map' and 'mapb'

--- main.go
+++ main.go
@@ -2,7 +2,10 @@ package main
 
 import (
 	"bufio"
+	"encoding/json"
 	"fmt"
+	"io"
+	"net/http"
 	"os"
 	"strings"
 )
@@ -11,29 +14,114 @@ import (
 type cliCommand struct {
 	name string
 	description string
-	callback func() error
+	callback func(p *Config) error
+}
+
+type Result struct {
+	Name string `json:"name"`
+	URL string `json:"url"`
+}
+
+type Config struct {
+	Count int `json:"count"`
+	Next string `json:"next"`
+	Previous string `json:"previous"`
+	Results []Result `json:"results"` //{
+		//Name string `json:"name"`
+		//URL string `json:"url"`
+	//} `json:"results"`
 }
 
 //ADD NEW COMMANDS TO MAP IN MAIN()
 
 //called on "exit" command
-func commandExit() error {
+func commandExit(p *Config) error {
 	fmt.Println("Closing the Pokedex... Goodbye!")
 	os.Exit(0)
 	return nil
 }
 
 //called on "help" command
-func commandHelp() error {
-	commandNames := []string{"exit", "help"}
-	commandDescriptions := []string{"Exit the Pokedex", "Displays a help message"}
+func commandHelp(p *Config) error {
+	commands := make(map[string]string)
+	commands["exit"] = "Exit the Pokedex"
+	commands["help"] = "Displays a help message"
+	commands["map"] = "Retrieves a group of locations"
+	commands["mapb"] = "Retrieves the previous group of locations"
+	//commandNames := []string{"exit", "help"}
+	//commandDescriptions := []string{"Exit the Pokedex", "Displays a help message"}
 	fmt.Println("Welcome to the Pokedex!")
 	fmt.Println("Usage:")
 	fmt.Println("")
-	for i, comm := range commandNames {
-		resp := fmt.Sprintf("%s: %s", comm, commandDescriptions[i])
+	for comm, scrip := range commands {
+		resp := fmt.Sprintf("%s: %s", comm, scrip)
 		fmt.Println(resp)
 	}
+	fmt.Println("")
+	return nil
+}
+
+//called on "map" command
+func commandMap(p *Config) error {
+	resp, err := http.Get(p.Next)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+
+	bod, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return err
+	}
+
+	con := Config{}
+	err = json.Unmarshal(bod, &con)
+	if err != nil {
+		return err
+	}
+	p.Next = con.Next
+	p.Previous = con.Previous
+	
+	for _, res := range con.Results {
+		fmt.Println(res.Name)
+	}
+
+	fmt.Println("")
+
+	return nil
+}
+
+//called on "mapb" command
+func commandMapB(p *Config) error {
+	if p.Previous == "null" {
+		fmt.Println("you're on the first page")
+		return nil
+	}
+	resp, err := http.Get(p.Previous)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+
+	bod, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return err
+	}
+
+	con := Config{}
+	err = json.Unmarshal(bod, &con)
+	if err != nil {
+		return err
+	}
+	p.Next = con.Next
+	p.Previous = con.Previous
+	
+	for _, res := range con.Results {
+		fmt.Println(res.Name)
+	}
+
+	fmt.Println("")
+
 	return nil
 }
 
@@ -46,7 +134,7 @@ func cleanInput(text string) []string {
 
 func main() {
 	input := bufio.NewScanner(os.Stdin)
-
+	param := Config{0, "https://pokeapi.co/api/v2/location-area/", "null", []Result {},}
 	commandList := map[string]cliCommand{
 	
 		"exit": {
@@ -59,6 +147,16 @@ func main() {
 			description: "Displays a help message",
 			callback: commandHelp,
 		},
+		"map": {
+			name: "map",
+			description: "Retrieves a group of locations",
+			callback: commandMap,
+		},
+		"mapb": {
+			name: "mapb",
+			description: "Retrieves the previous group of locations",
+			callback: commandMapB,
+		},
 	}
 	for ;; {
 		fmt.Print("Pokedex > ")
@@ -67,7 +165,7 @@ func main() {
 		found := false
 		for _, comm := range commandList {
 			if command == comm.name {
-				comm.callback()
+				comm.callback(&param)
 				found = true
 			}
 		}
